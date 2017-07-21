dd0c47d223c6e1a5d8ab481c9133acc6537dde68
hezone <183844707@qq.com>
2017-07-21 14:22:43 +0800

    news

--- data/cache/filecache/u1RWXf/forumdisplay/2/u1RWXf_forumdisplay_2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = array (
-  'exp' => 1500615175,
-  'data' => 
-  array (
-  ),
-);
--- data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 443b242d21ffb5a25035eeb0067fc46a
-
-$pluginsetting = array (
-);
-?>
